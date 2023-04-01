98a630ed6012a11a51176ba52d5551c1d1abba16
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-01 09:15:43 -0400

    Task 1 completed

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+console.log(`======================Task-1===========================`);
+
+//variable for total number of categories
+const numberOfCat = document.querySelector("ul#categories").children.length;
+
+//variables for the names of categories and total nubmer of elements
+const nameOfTheCategory = document.querySelectorAll("li.item h2");
+const numberOfElements = document.querySelectorAll("li.item ul");
+
+//displaying total number of categories
+console.log(`Number of categories: ${numberOfCat}`);
+console.log("\n");
+
+//iterating and displaying categories and total number of elements
+for (let i = 0; i < numberOfCat; i += 1) {
+  console.log(`Category: ${nameOfTheCategory[i].textContent}`);
+  console.log(`Elements ${numberOfElements[i].children.length}`);
+  console.log("\n");
+}
+

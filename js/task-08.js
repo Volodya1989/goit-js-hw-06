7f7a5ed928de655e8813c34a6c19973587148ded
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-04 16:23:11 -0400

    task8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const refs = {
+  form: document.querySelector("form.login-form"),
+};
 
+const onSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    event.preventDefault();
+    window.alert("Please fill in all the fields!");
+  }
+  const newObj = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(`FormData: ${JSON.stringify(newObj)}`);
+
+  event.currentTarget.reset();
+};
+
+refs.form.addEventListener("submit", onSubmit);

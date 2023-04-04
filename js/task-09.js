9084036506815b1d89ea6343e90df260139bd232
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-04 16:30:30 -0400

    task9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector("button.change-color"),
+};
+const onClickBackgroundColor = () => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+};
+refs.body.addEventListener("click", onClickBackgroundColor);

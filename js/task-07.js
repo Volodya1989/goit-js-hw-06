010f1f4b99ddec9b93283cc958d20fb0d0f53316
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-05 11:21:58 -0400

    task7 corrected

--- js/task-07.js
+++ js/task-07.js
@@ -3,8 +3,12 @@ const refs = {
   colorPicker: document.querySelector("input#font-size-control"),
 };
 
+const onLoad = () => {
+  refs.textToChange.style.fontSize = `${refs.colorPicker.value}px`;
+};
 const onInputPicker = (event) => {
   refs.textToChange.style.fontSize = `${event.currentTarget.value}px`;
 };
 
+window.addEventListener("load", onLoad);
 refs.colorPicker.addEventListener("input", onInputPicker);
